a005beaa1ff19beb89b93d4c7377c98f535615d0
tejalmishra88 <tejalmishra88@gmail.com>
2023-04-11 19:51:26 +1000

    sidebar titles are showing up

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {useState, useEffect} from "react";
 import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
 
 const Feed = () => {
   return (
    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        
+        <Sidebar />
+     <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 Tejal Media
+        </Typography>  
      </Box>
 
    </Stack>
