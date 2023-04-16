02a61a2b30daf6b73aa1bd9b3f5baa2d391afcab
tejalmishra88 <tejalmishra88@gmail.com>
2023-04-16 18:29:47 +1000

    video name search is working

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack } from '@mui/material';
+import { ChannelCard, Loader, VideoCard } from "./";
 
-const Videos = () => {
+const Videos = ({videos}) => {
+    if(!videos?.length) return <Loader />;
   return (
-    <div>Videos</div>
+    <Stack direction="row" flexWrap="wrap"
+    justifyContent="start" gap={2}> 
+     {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
